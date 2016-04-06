829182fe52ed51700be9e6537d45b4ba437914cc
frankjoshua <frankjoshua@gmail.com>
2016-04-06 01:02:25 -0500

    Update rosdocker.sh

--- rosdocker.sh
+++ rosdocker.sh
@@ -1,1 +1,1 @@
-docker run -it --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="QT_X11_NO_MITSHM=1"  osrf/ros:jade-desktop-full bash
+docker run -it --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="QT_X11_NO_MITSHM=1"  frankjoshua/ros-test:latest bash
