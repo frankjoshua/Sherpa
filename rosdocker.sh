555ac44a988822418bb17a6d2b073643c3e89e5b
frankjoshua <frankjoshua@gmail.com>
2016-04-07 22:31:04 -0500

    Update rosdocker.sh

--- rosdocker.sh
+++ rosdocker.sh
@@ -1,1 +1,1 @@
-docker run -it --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="QT_X11_NO_MITSHM=1"  frankjoshua/ros-test:latest bash
+xhost + & docker run -it --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="QT_X11_NO_MITSHM=1"  frankjoshua/ros-test:latest bash
