eae455e644dc3cd5bde184097da2030830b7b8eb
Joshua Frank <frankjoshua@gmail.com>
2016-04-14 03:27:52 +0000

    Added raspberry pi serial

--- docker/startup-scripts/rosserial.sh
+++ docker/startup-scripts/rosserial.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -itd --privileged --net ros --name serial --device=/dev/ttyACM0 --env ROS_HOSTNAME=serial --env ROS_MASTER_URI=http://master:11311 frankjoshua/ros-serial
+docker run -itd --privileged --net ros --name serial --device=/dev/ttyACM0 --env ROS_HOSTNAME=serial --env ROS_MASTER_URI=http://master:11311 frankjoshua/${DOCKER_PREFIX}ros-serial $@
