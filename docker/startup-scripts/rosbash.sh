3d32945164c05433df83fedfb757b2d88dd0db81
Joshua Frank <frankjoshua@gmail.com>
2016-05-08 01:00:33 -0500

    Updates

--- docker/startup-scripts/rosbash.sh
+++ docker/startup-scripts/rosbash.sh
@@ -2,4 +2,4 @@
 xhost +
 #generate random name
 UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 3 | head -n 1)
-docker run -it --net ros --name ros-bash-$UUID --env="DISPLAY" --volume="$HOME:$HOME" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="QT_X11_NO_MITSHM=1" --env ROS_HOSTNAME=ros-bash-$UUID --env ROS_MASTER_URI=http://master:11311 frankjoshua/ros-bash
+nvidia-docker run -it --privileged --net ros --name ros-bash-$UUID --env="DISPLAY" --volume="$HOME:$HOME" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="QT_X11_NO_MITSHM=1" --env ROS_HOSTNAME=ros-bash-$UUID --env ROS_MASTER_URI=http://master:11311 frankjoshua/ros-bash
