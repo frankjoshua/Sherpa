7bfad852876df2706882648e4ed9bf0fdd4acbf5
Joshua Frank <josh@tesseractmobile.com>
2016-05-28 23:14:17 -0500

    Added launch files

--- docker/startup-scripts/rosbash.sh
+++ docker/startup-scripts/rosbash.sh
@@ -3,4 +3,4 @@ eval $(weave env)
 xhost +
 #generate random name
 UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 3 | head -n 1)
-docker run -it --rm --privileged --name ros-bash-$UUID --env="DISPLAY" --volume="$HOME:$HOME" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="QT_X11_NO_MITSHM=1" --env ROS_HOSTNAME=ros-bash-$UUID --env ROS_MASTER_URI=http://master:11311 --env ROS_HOME=$HOME -w $HOME/efim/ros_ws/src/efim frankjoshua/ros-bash $@
+docker run -it --rm --privileged --name ros-bash-$UUID --env="DISPLAY" --volume="$HOME:$HOME" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="QT_X11_NO_MITSHM=1" --env ROS_HOSTNAME=ros-bash-$UUID --env ROS_MASTER_URI=http://master:11311 --env ROS_HOME=$HOME -w $HOME frankjoshua/ros-bash $@
