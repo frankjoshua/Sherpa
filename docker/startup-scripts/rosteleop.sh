c6318ef49eb4af995fa797a492207cf15a23c2d7
root <root@ros-bash-rJd.weave.local>
2016-05-30 03:04:12 +0000

    updates

--- docker/startup-scripts/rosteleop.sh
+++ docker/startup-scripts/rosteleop.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --rm -it --net ros --name teleop --env ROS_HOSTNAME=teleop --env ROS_MASTER_URI=http://master:11311 frankjoshua/ros-teleop $@
+eval $(weave env)
+docker run --rm -it --name teleop --env ROS_HOSTNAME=teleop --env ROS_MASTER_URI=http://master:11311 frankjoshua/ros-teleop $@
---
+++ rosbash.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export SHERPA_PATH=./docker/startup-scripts
+$SHERPA_PATH/rosbash.sh
