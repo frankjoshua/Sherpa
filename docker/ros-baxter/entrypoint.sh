a3acb6bc8cc97131d827c80b1d4a0ae84dd6774e
Joshua Frank <josh@tesseractmobile.com>
2016-11-02 00:41:09 -0500

    Added ROS Baxter sim

---
+++ docker/ros-baxter/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# setup ros environment
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+cd "/root/ros_ws"
+./baxter.sh sim &
+roslaunch baxter_gazebo baxter_world.launch
+exec "$@"
---
+++ docker/ros-baxter/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker run -it \
+  --privileged \
+  --env="DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  -e your_ip=127.0.0.1 \
+  -e baxter_hostname=baxter.local \
+  frankjoshua/ros-baxter $@
